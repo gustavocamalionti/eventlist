e944d693ed545184ccde8e885bf9364efa5f2a86
Gustavo Camalionti <gustavocamalionti@gmail.com>
2025-06-09 23:15:02 -0300

    feat: add `tenancyForLaravel` and config module

---
+++ database/migrations/tenant/2025_06_09_234719_create_vouchers_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 155)->comment("Nome do Ingresso");
+            $table->tinyInteger("active")->default(false)->comment("Indica se o Ingresso está ativo ou não");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
