dde16125cf8f97ef6d3890cfb7daedcd8958d62d
Gustavo Camalionti <gustavo.gomes@halipar.com.br>
2025-06-18 14:26:18 -0300

    style: remove desnecessary refers

--- app/Models/Systems/Tenant/User.php
+++ app/Models/Systems/Tenant/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models\Tenant;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
